9d0e9652cdea35c96941bbaf7f002e3a335eeb2b
chinaliuweijie <chinaliuweijie@163.com>
2017-06-08 11:06:28 +0800

    Rejected commit: Default

--- app/src/main/java/com/qingmiao/terroristwatch/MainActivity.java
+++ app/src/main/java/com/qingmiao/terroristwatch/MainActivity.java
@@ -22,8 +22,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +36,7 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
